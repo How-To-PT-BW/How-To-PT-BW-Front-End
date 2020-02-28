ac4b36c28363f641fbf54ed22e7f210582908d20
Jeff Breig <jjbreig@gmail.com>
2020-02-27 22:03:17 -0500

    added some variables

---
+++ my-app/src/styledComponents/variables.js
@@ -0,0 +1,26 @@
+//**MEDIA QUERY DEVICE SIZES**//
+const size = {
+    mobile: '500px',
+    tablet: '800px',
+    desktop: '1000px'
+}
+export const device = {
+    mobile: `(min-width: ${size.mobile})`,
+    tablet: `(min-width: ${size.tablet})`,
+    desktop: `(min-width: ${size.desktop})`
+};
+
+export const colors = {
+    primary: '#FDBA77',
+    text1:'#E6EDF4',
+    text2:'#D4E0ED',
+    text3:'#8cabcf',
+    text4: '#4989ae',
+    text5: '#2a4565',
+    background1: '#ffffff',
+    background2: 'f1f9f9',
+    background3: 'a8dbd8',
+    background4: '61bdb8',
+    background5: '36827e'
+};
+
