23ec43215a8f19c9a2f1b320768f9bec99cc6c0e
Steven Waldron <stevenwaldron@Stevens-MBP.home>
2020-03-04 21:26:58 -0500

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Fragment }from 'react';
 import './App.css';
 import LoginForm from './components/Login';
 import SignUpForm from './components/Signup';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import ProtectedRoute from './components/ProtectedRoute';
 import DraftForm from './components/DraftAnArticle';
+import TopBarBlock from './styledComponents/topbarblock';
+import Step from './components/Step';
+
 
 // staging
 
 function App() {
+  const [steps,setSteps] = useState([])
   return (
+    
+    
     <Router>
+      <TopBarBlock/>
       <nav>
         <Link to="/login">Login</Link>
         <Link to="/signup">Sign Up</Link>
+        <Link to="/DraftAnArticle">Create</Link>
       </nav>
       <div>
         <Switch>
           <Route path="/login" component={LoginForm} />
           <Route path="/signup" component={SignUpForm} />
           <ProtectedRoute exact path="/draft" component={DraftForm} />
+          <Route path="/DraftAnArticle" render={props => <DraftForm {...props}/>}/>
+          <Route exact path="/Step/:id" render={props => <Step {...props} steps={steps} setSteps={setSteps}/>}/>
         </Switch>
       </div>
     </Router>
+    
   );
 }
 
--- my-app/src/components/DraftAnArticle.js
+++ my-app/src/components/DraftAnArticle.js
@@ -1,61 +1,108 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import styled from "styled-components";
-import { colors, devices } from "../styledComponents/variables";
-import { useForm } from "react-hook-form";
-import { H2 } from "../styledComponents/fontStyles";
-import DraftArticleLogo from "../images/DraftArticleLogo.png";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import styled from "styled-components";
+// import { colors, devices } from "../styledComponents/variables";
+// import { useForm } from "react-hook-form";
+// import { H2 } from "../styledComponents/fontStyles";
+// import DraftArticleLogo from "../images/DraftArticleLogo.png";
 
-const Container = styled.div`
-  background-color: ${colors.background3};
-  fill: solid ${colors.background3};
-  border: 1px solid red;
+// const Container = styled.div`
+//   background-color: ${colors.background3};
+//   fill: solid ${colors.background3};
+//   border: 1px solid red;
 
-  @media ${devices.mobile} {
-    max-width: 500px;
-  }
-  @media ${devices.tablet} {
-    max-width: 800px;
-  }
-  @media ${devices.desktop} {
-    max-width: 1024px;
-  }
-`;
+//   @media ${devices.mobile} {
+//     max-width: 500px;
+//   }
+//   @media ${devices.tablet} {
+//     max-width: 800px;
+//   }
+//   @media ${devices.desktop} {
+//     max-width: 1024px;
+//   }
+// `;
 
-const DraftHeader = styled.div`
-  background-image: url(${DraftArticleLogo});
-  background-repeat: no-repeat;
-  background-position: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 50px;
-`;
+// const DraftHeader = styled.div`
+//   background-image: url(${DraftArticleLogo});
+//   background-repeat: no-repeat;
+//   background-position: center;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 50px;
+// `;
 
-function DraftForm() {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => {
-    console.log(data);
-  }; // your form submit function which will invoke after successful validation
+// function DraftForm() {
+//   const { register, handleSubmit, watch, errors } = useForm();
+//   const onSubmit = data => {
+//     console.log(data);
+//   }; // your form submit function which will invoke after successful validation
 
-  return (
-    <Container>
-      <DraftHeader>
-        <H2>Draft an Article</H2>
-      </DraftHeader>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Example</label>
-        <input name="example" defaultValue="test" ref={register} />
-        <label>ExampleRequired</label>
-        <input
-          name="exampleRequired"
-          ref={register({ required: true, maxLength: 10 })}
-        />
-        {errors.exampleRequired && <p>This field is required</p>}
-        <input type="submit" />
-      </form>
-    </Container>
-  );
+//   return (
+//     <Container>
+//       <DraftHeader>
+//         <H2>Draft an Article</H2>
+//       </DraftHeader>
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <label>Example</label>
+//         <input name="example" defaultValue="test" ref={register} />
+//         <label>ExampleRequired</label>
+//         <input
+//           name="exampleRequired"
+//           ref={register({ required: true, maxLength: 10 })}
+//         />
+//         {errors.exampleRequired && <p>This field is required</p>}
+//         <input type="submit" />
+//       </form>
+//     </Container>
+//   );
+// }
+
+// export default DraftForm;
+
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+
+
+const DraftForm = (props) => {
+    const { register, handleSubmit, setValue, errors } = useForm()
+    const id = Number(1)
+
+    const submit = (data) => {
+        data.instructions = []
+        console.log(data)
+        setValue('title','')
+        setValue('problem','')
+        setValue('solution','')
+        axios.post("/how-to", {data})
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    
+   
+    return (
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <label>title</label>
+                <input type='text' placeholder='title' name='title' ref={register({required: "TITLE REQUIRED"})}></input>
+                {errors.title && <p>{errors.title.message}</p>}
+                <label>problem</label>
+                <input type='text' placeholder='problem' name='problem' ref={register({required: "PROBLEM REQUIRED"})}></input>
+                {errors.problem && <p>{errors.problem.message}</p>}
+                <label>solution</label>
+                <input type='text' placeholder='solution' name='solution' ref={register({required: "SOLUTION REQUIRED"})}></input>
+                {errors.solution && <p>{errors.solution.message}</p>}
+                <button onClick={() => props.history.push(`/Step/${id}`)}>next step</button>
+                <button>save draft</button>
+            </form>
+            
+        </div>
+    );
 }
 
 export default DraftForm;
