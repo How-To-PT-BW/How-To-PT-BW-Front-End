6580d89525a8b73f352f4c171b15753484c142d8
Jeff Breig <jjbreig@gmail.com>
2020-03-05 21:13:04 -0500

    added edit button, need to add functionality

--- my-app/src/components/HowTo.js
+++ my-app/src/components/HowTo.js
@@ -1,5 +1,8 @@
 import React, { useState, Fragment, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
+import { colors, devices } from "../styledComponents/variables";
+import { useForm } from "react-hook-form";
 
 function HowTo(props) {
     const [article, setArticle] = useState({});
@@ -8,22 +11,33 @@ function HowTo(props) {
     const editArticle = article =>{
         setEditing(true);
     };
-      const saveEdit = e => {
-
-        console.log("submitted!");
-
-        // e.preventDefault();
-        // axiosWithAuth()
-        //   .put(`/how-to/${props.match.params.id}`, data)
-        //   .then(res => {
-        //     console.log(res);
-        //     colorChange(colors);
-        //   })
-        //   .catch(err => {
-        //     console.log(err);
-        //   });
-      };
-
+const { register, handleSubmit, watch, errors } = useForm();
+const onSubmit = data => {
+  console.log(data);
+//   axiosWithAuth()
+//     .post("/users/login", data)
+//     .then(res => {
+//       console.log(res);
+//       localStorage.setItem("token", res.data.token);
+//       props.history.push("/draft");
+//     })
+//     .catch(err => {
+//       localStorage.removeItem("token");
+//       console.log("There was an error", err);
+//     });
+};
+const FormSubmit = styled.input`
+  background-color: ${colors.primary};
+  padding: 0px 25px;
+  border: none;
+  border-radius: 36.5px;
+  font-size: 28px;
+  font-family: "Martel";
+  font-weight: 700;
+  color: white;
+  width: 163px;
+  height: 48px;
+`;
     useEffect(function getArticle() {
       axios
         .get(`https://how-to-lifehack.herokuapp.com/how-to/${props.match.params.id}`)
@@ -37,28 +51,36 @@ function HowTo(props) {
         });
     }, []);
     console.log(props);
+    
 
     return (
       <div>
         <h1>{article.title}</h1>
         <h1>{article.problem}</h1>
+        <button onClick={editArticle}>Edit</button>
         {editing && (
-          <form onSubmit={saveEdit}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <legend>edit article</legend>
             <label>
               title
               <input
                 value={article.title}
+                name="title"
+                type="text"
+                ref={register}
               />
             </label>
             <label>
               problem
               <input
                 value={article.problem}
+                name="problem"
+                type="text"
+                ref={register}
               />
             </label>
             <div className="button-row">
-              <button type="submit">save</button>
+              <FormSubmit type="submit" value="Submit" />
               <button onClick={() => setEditing(false)}>cancel</button>
             </div>
           </form>
