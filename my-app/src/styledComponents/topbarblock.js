94f751ea375f09a42615b2807f71330712ad9ec0
Jeff Breig <jjbreig@gmail.com>
2020-03-08 22:50:20 -0400

    dynamic login and styled top bar more

--- my-app/src/styledComponents/topbarblock.js
+++ my-app/src/styledComponents/topbarblock.js
@@ -1,8 +1,9 @@
 import React, {useContext} from 'react';
 import styled from 'styled-components';
 import {UserContext} from '../utilities/userContext';
+import {colors} from "./variables";
 const Topbarbackground = styled.div`
-    background: #36827E;
+    background: ${colors.background5};
     height: 100px;
     display: flex;
     justify-content: space-evenly;
@@ -24,19 +25,38 @@ const Topbartext = styled.h3`
     text-align: center;
     letter-spacing: 0.01px;
 
-    color: #F1F9F9;
+    color: ${colors.background2};
 `
+
+const UserText = styled.h4`
+  font-family: Martel;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 32px;
+
+  text-align: center;
+  letter-spacing: 0.01px;
+
+  color: ${colors.background2};
+`;
 
 
 function Topbarblock() {
     const user = useContext(UserContext);
 
     return (
-        <Topbarbackground>
-            <div></div>
-            <Topbartext>How-To</Topbartext>
-            <h3>Welcome {user.user}</h3>
-        </Topbarbackground>
+      <Topbarbackground>
+        <div></div>
+        <Topbartext>How-To</Topbartext>
+        {user.loggedIn ? (
+          <UserText>Welcome {user.user}</UserText>
+        ) : (
+          <UserText>
+            Please <a href="/login">Log In</a>
+          </UserText>
+        )}
+      </Topbarbackground>
     );
 }
 
